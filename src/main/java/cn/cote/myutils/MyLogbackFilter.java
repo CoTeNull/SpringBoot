1b9622c7e3ae6ddcbe939c4a7f7228b704931e67
CoTeNull <895449345@qq.com>
2018-08-23 10:19:33 +0800

    logback join in

---
+++ src/main/java/cn/cote/myutils/MyLogbackFilter.java
@@ -0,0 +1,18 @@
+package cn.cote.myutils;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.filter.Filter;
+import ch.qos.logback.core.spi.FilterReply;
+
+public class MyLogbackFilter extends Filter<ILoggingEvent> {
+
+    @Override
+    public FilterReply decide(ILoggingEvent iLoggingEvent) {
+
+       if(iLoggingEvent.getLoggerName().equals("upload")){
+            return FilterReply.ACCEPT;
+       }else {
+           return FilterReply.DENY;
+       }
+    }
+}
